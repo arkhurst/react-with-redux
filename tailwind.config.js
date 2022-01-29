095c4ecfe4477892ba570ddd50c1a276dc17cef2
arkhurst <fiiarkhurst@gmail.com>
2022-01-29 13:31:44 +0000

    added tailwind config and custom font

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      blue: colors.blue,
+      gray: colors.slate,
+      white: colors.white,
+      red: colors.red,
+      yellow: colors.yellow,
+    },
+    fontFamily: {
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
